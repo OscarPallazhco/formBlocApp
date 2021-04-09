2020edada6ba4e33f438cf25cd505bde47a401f8
Oscar Pallazhco <oepallaz@gmail.com>
2021-04-08 21:59:10 -0500

    validaciones como transforms de los streams

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
-class LoginBloc{
+import 'package:formbloc_app/bloc/validators.dart';
+
+class LoginBloc with Validators{
 
   final _emailCtrller = StreamController<String>.broadcast();
   final _passwordCtrller = StreamController<String>.broadcast();
 
-  Stream<String> get emailStream => _emailCtrller.stream;
-  Stream<String> get passwordStream => _passwordCtrller.stream;
+  Stream<String> get emailStream => _emailCtrller.stream.transform(validateEmail);
+  Stream<String> get passwordStream => _passwordCtrller.stream.transform(validatePassword);
 
   Function(String) get changeEmail => _emailCtrller.sink.add;
   Function(String) get changePassword => _passwordCtrller.sink.add;
