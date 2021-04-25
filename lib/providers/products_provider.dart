db4acc604196334c1762c8a06bfa8f3be61ce017
Oscar Pallazhco <oepallaz@gmail.com>
2021-04-25 16:21:01 -0500

    .

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -24,27 +24,27 @@ class ProductsProvider {
     }
   }
 
-    Future<List<ProductModel>> getProducts() async{
-      try {
-        final String productsUrl = '$databaseUrl/products.json';
-        final url = Uri.parse(productsUrl);
-        final resp = await http.get(url);
+  Future<List<ProductModel>> getProducts() async{
+    try {
+      final String productsUrl = '$databaseUrl/products.json';
+      final url = Uri.parse(productsUrl);
+      final resp = await http.get(url);
 
-        final Map<String, dynamic> decodedData = json.decode(resp.body);
-        // print(decodedData);
-        if (decodedData == null ) return [];
-        List<ProductModel> products = [];
-        decodedData.forEach((key, value) {
-          ProductModel prodTemp = ProductModel.fromJson(value);
-          prodTemp.id = key;
-          products.add(prodTemp);
-        });
-        return products;
-      } catch (e) {
-        print('error en getProducts');
-        print(e);
-        return [];
-      }
-  }
+      final Map<String, dynamic> decodedData = json.decode(resp.body);
+      // print(decodedData);
+      if (decodedData == null ) return [];
+      List<ProductModel> products = [];
+      decodedData.forEach((key, value) {
+        ProductModel prodTemp = ProductModel.fromJson(value);
+        prodTemp.id = key;
+        products.add(prodTemp);
+      });
+      return products;
+    } catch (e) {
+      print('error en getProducts');
+      print(e);
+      return [];
+    }
+}
   
 }
